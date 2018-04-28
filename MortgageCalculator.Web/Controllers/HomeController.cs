7c9c9cb63a4afcc1473e07f99aba7dfbf6ab8a9f
kvprasanna <kvprasanna1084@gmail.com>
2018-04-28 15:45:36 +1000

    BugFix and display mortgage
    bugfix in service and display

---
+++ MortgageCalculator.Web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MortgageCalculator.Dto;
+namespace MortgageCalculator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+            // GET: Mortgage details
+            public async Task<ActionResult> Index()
+            {
+                string apiUrl = "http://localhost:49608/api/Mortgage";
+                var mortgages = new List<Mortgage>();
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(apiUrl);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var data = await response.Content.ReadAsStringAsync();
+                        mortgages = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Mortgage>>(data);
+                        
+                }
+
+
+                }
+
+                return View(mortgages);
+
+            }
+        
+    }
+}
